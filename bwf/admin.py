6a95d453489ac08c43b8db92182174df4517450b
markwithk <markxu18@gmail.com>
2014-12-12 21:49:15 -0500

    use admin interface

--- bwf/admin.py
+++ bwf/admin.py
@@ -1,13 +1,25 @@
-
 from django import forms
 from django.contrib import admin
 
 from bwf.models import User, Friend,  Bill
 
 admin.site.register(User)
-
-
 admin.site.register(Friend)
 
+class BillAdminForm(forms.ModelForm):
+    class Meta:
+        model=Bill
+        fields = '__all__'
 
-admin.site.register(Bill)
+    def __init__(self, *args, **kwargs):
+        super(BillAdminForm, self).__init__(*args, **kwargs)
+
+        user_choices = tuple( (each.email, "user: "+ each.get_full_name()+"  "+each.email) for each in  User.objects.all() )
+        friend_choices = tuple( (each.email, "friend: "+ each.get_full_name()+"  "+each.email) for each in  Friend.objects.all() )
+
+        self.fields['creditor'] = forms.ChoiceField( user_choices+friend_choices )
+        self.fields['debtor'] = forms.ChoiceField( user_choices+friend_choices )
+
+class BillAdmin(admin.ModelAdmin):
+    form = BillAdminForm
+admin.site.register(Bill, BillAdmin)
