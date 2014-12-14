2df2cfcbf8e1e6b1f64070ad05fdb0241afaebb1
markwithk <markxu18@gmail.com>
2014-12-14 17:37:49 -0500

    add charts and carousel

---
+++ hciproject/static/js/chart.js
@@ -0,0 +1,52 @@
+var chart = {};
+
+chart.draw = function (id, data) {
+    console.log(id);
+    console.log(data);
+    $('#'+id ).highcharts({
+        chart: {
+            plotBackgroundColor: null,
+            plotBorderWidth: 0,
+            plotShadow: false
+        },
+        title: {
+            text: 'Browser<br>shares',
+            align: 'center',
+            verticalAlign: 'middle',
+            y: 0
+        },
+        tooltip: {
+            pointFormat: '{series.name}: <b>{point.percentage:.1f}%</b>'
+        },
+        plotOptions: {
+            pie: {
+                 events: {
+                    click: function (e) {
+                        console.log("click on pie: "+e.point);
+                        window.location.href = "/friend/"+e.point.friend_pk;
+                    }
+                },
+                allowPointSelect: true,
+                cursor: 'pointer',
+                dataLabels: {
+                    enabled: true,
+                    distance: -50,
+                    style: {
+                        fontWeight: 'bold',
+                        color: 'white',
+                        textShadow: '0px 1px 2px black'
+                    }
+                },
+                center: ['50%', '50%']
+            }
+        },
+        
+        series: [{
+            type: 'pie',
+            name: 'Browser share',
+            innerSize: '45%',
+            data: data
+        }]
+    });
+};
+
